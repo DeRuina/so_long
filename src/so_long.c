0ed1b1f190fe9bce21739f1db27439a625ee65e9
Dean Ruina <druina@c2r5p11.hive.fi>
2023-03-09 16:04:21 +0200

    not working still playing with the library

--- src/so_long.c
+++ src/so_long.c
@@ -6,63 +6,128 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 09:38:45 by druina            #+#    #+#             */
-/*   Updated: 2023/03/08 10:11:35 by druina           ###   ########.fr       */
+/*   Updated: 2023/03/09 16:03:41 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp; /* bits per pixel */
+	int		line_len;
+	int		endian;
+}	t_img;
+
 typedef struct  s_program
 	{
     	void *mlx;
-   		 void *win;
+   		void *win;
+		t_img img;
 	}               t_program;
 
-int key_handler(int key, void *param)
+void img_pix_put(t_img *img, int x, int y, int color)
+{
+	char *pixel;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
+}
+
+int key_handler(int key, t_program *temp)
 {
 	int i;
-	t_program *temp;
 
-	temp = param;
 
 
 	i = 100;
 	if (key == 124)
 	{
 	 while (i-- != 0)
-		mlx_pixel_put(temp->mlx, temp->win, i, 100, 0x2E9E2A);
+		img_pix_put(&temp->img, i, 100, 0x2E9E2A);
 	}
 	if (key == 125)
 	{
 	while (i++ != 200)
-		mlx_pixel_put(temp->mlx, temp->win, 50, i, 0x2E9E2A);
+		img_pix_put(&temp->img, 50, i, 0x2E9E2A);
 	}
+	return (0);
+}
+
+int mouse_handler(int key, void *param)
+{
+
+
+	param = NULL;
+	if (key == 1)
+		ft_printf("BOOM\n");
+	if (key == 2)
+		ft_printf("CHAKALAK\n");
+	return(0);
+}
+
+int mouse_movement_handler(int x, int y, t_program *temp)
+{
+
+	img_pix_put(&temp->img, x, y, 0xE8F70E);
+	ft_printf("location is %d.%d\n", x, y);
+	return(0);
+}
+
+void	render_background(t_img *img, int color)
+	{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 1000)
+	{
+		j = 0;
+		while (j < 1000)
+			img_pix_put(img, j++, i, color);
+		++i;
+	}
+	}
+
+t_img create_image(t_program *program)
+{
+	t_img image;
+
+	image->mlx_img = mlx_new_image(program->mlx, 1000, 1000);
+	image->addr = mlx_get_data_addr(image->addr, &image->bpp, &image->line_len, &image->endian);
+	return (image);
+}
+
+int render(t_program *program)
+{
+	if (program->win == NULL)
+		return (1);
+	program->img = create_image(program);
+	render_background(&program->img, 0x3B18EC);
+	mlx_put_image_to_window(program->mlx, program->win, program->img.mlx_img, 0, 0);
+	// mlx_destroy_image(program->mlx, program->img.mlx_img);
+	// program->img = create_image(program);
+	// render_background(&program->img, 0xE8F70E);
+	// mlx_put_image_to_window(program->mlx, program->win, program->img.mlx_img, 0, 0);
 	return (0);
 }
 
 int	main(void)
 {
-	void *mlx_ptr;
-	void *win_ptr;
 
-	// int i;
-
-	// i = 100;
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "so long");
-	// while (i-- != 0)
 	t_program program;
-	program.mlx = mlx_ptr;
-	program.win = win_ptr;
-	// 	mlx_pixel_put(mlx_ptr, win_ptr, i, 100, 0x2E9E2A);
-	// i = 100;
-	// while (i++ != 200)
-	// 	mlx_pixel_put(mlx_ptr, win_ptr, 50, i, 0x2E9E2A);
-	// mlx_pixel_put(mlx_ptr, win_ptr, 1, 1, 0xffffff);
-	// mlx_pixel_put(mlx_ptr, win_ptr, 101, 100, 0xffffff);
-	// mlx_pixel_put(mlx_ptr, win_ptr, 100, 101, 0xffffff);
-	mlx_string_put(mlx_ptr, win_ptr, 20, 20, 0xF22727, "hello");
-	mlx_key_hook(win_ptr, &key_handler, &program);
-	mlx_loop(mlx_ptr);
+
+
+	program.mlx  = mlx_init();
+	program.win = mlx_new_window(program.mlx, 1000, 1000, "so long");
+	// program.img.mlx_img = mlx_new_image(program.mlx, 1000, 1000);
+	// program.img.addr = mlx_get_data_addr(program.img.mlx_img, &program.img.bpp, &program.img.line_len, &program.img.endian);
+
+	// mlx_key_hook(program.win, &key_handler, &program);
+	mlx_mouse_hook(program.win, &mouse_handler, &program);
+	// mlx_hook(program.win, 6, 1L<<6, mouse_movement_handler, &program);
+	mlx_loop_hook(program.mlx, &render, &program);
+	mlx_loop(program.mlx);
 	return (0);
 }
